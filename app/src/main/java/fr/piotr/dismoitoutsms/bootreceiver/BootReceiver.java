8b810b7ede1606ef4ff8a6a57be43b398a598574
piotr <piotr.fleury@gmail.com>
2017-07-09 20:48:24 +0200

    add service and headset receiver

--- app/src/main/java/fr/piotr/dismoitoutsms/bootreceiver/BootReceiver.java
+++ app/src/main/java/fr/piotr/dismoitoutsms/bootreceiver/BootReceiver.java
@@ -16,7 +16,6 @@ public class BootReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         if(intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)){
-            //TODO start service if needed
             context.startService(new Intent(context, DisMoiToutSmsService.class));
         }
     }

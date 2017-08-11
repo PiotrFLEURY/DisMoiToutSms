d4a33f992f8bb2d01420f954709e179268ab0f6a
piotr <piotr.fleury@gmail.com>
2017-08-11 11:54:20 +0200

    bugfix bluettooth device connection

--- app/src/main/java/fr/piotr/dismoitoutsms/headset/BluetoothReceiver.java
+++ app/src/main/java/fr/piotr/dismoitoutsms/headset/BluetoothReceiver.java
@@ -1,9 +1,13 @@
 package fr.piotr.dismoitoutsms.headset;
 
+import android.bluetooth.BluetoothA2dp;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothHeadset;
+import android.bluetooth.BluetoothProfile;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Handler;
 
 /**
  * Created by piotr on 08/08/2017.
@@ -12,30 +16,111 @@ import android.content.Intent;
 
 public class BluetoothReceiver extends AbstractHeadSetReceiver {
 
+    final Handler handler = new Handler();
+
     @Override
     public void onReceive(Context context, Intent intent) {
-        switch(intent.getAction()){
-            case BluetoothDevice.ACTION_ACL_CONNECTED:
-                onHeadSetPluggedIn(context);
+        BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+        if(device != null) {
+            switch(intent.getAction()){
+                case BluetoothDevice.ACTION_ACL_CONNECTED:
+                    postDelayCheck(context);
+                    break;
+                case BluetoothDevice.ACTION_ACL_DISCONNECTED:
+                    checkBluetoothHeadSetState(context);
+                    break;
+                case BluetoothAdapter.ACTION_STATE_CHANGED:
+                    final int state = intent.getIntExtra(BluetoothAdapter.EXTRA_STATE, BluetoothAdapter.ERROR);
+                    onStateChanged(state);
+                    break;
+            }
+        }
+
+
+    }
+
+    private void postDelayCheck(final Context context) {
+        handler.postDelayed(() -> checkBluetoothHeadSetState(context), 3000);
+    }
+
+    private void onStateChanged(int state) {
+        switch(state) {
+            case BluetoothAdapter.STATE_OFF:
                 break;
-            case BluetoothDevice.ACTION_ACL_DISCONNECTED:
-                onHeadSetPluggedOut(context);
+            case BluetoothAdapter.STATE_TURNING_OFF:
                 break;
-            /*case BluetoothAdapter.ACTION_STATE_CHANGED:
-                onStateChanged(context, intent);
-                break;*/
+            case BluetoothAdapter.STATE_ON:
+                break;
+            case BluetoothAdapter.STATE_TURNING_ON:
+                break;
         }
     }
 
-    /*public void onStateChanged(Context context, Intent intent) {
-        BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-        if(device != null) {
-            if (device.getBondState()== BluetoothDevice.BOND_BONDED) {
-                onHeadSetPluggedIn(context);
-            } else {
-                onHeadSetPluggedOut(context);
+    private void checkBluetoothHeadSetState(Context context) {
+
+        // Get the default adapter
+        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        checkHeadsetProfile(context, mBluetoothAdapter);
+
+    }
+
+    private void checkHeadsetProfile(final Context context, final BluetoothAdapter mBluetoothAdapter) {
+        BluetoothProfile.ServiceListener mHeadsetProfileListener = new BluetoothProfile.ServiceListener() {
+
+            @Override
+            public void onServiceConnected(int profile, BluetoothProfile proxy) {
+                if (profile == BluetoothProfile.HEADSET) {
+                    BluetoothHeadset mBluetoothHeadset = (BluetoothHeadset) proxy;
+                    boolean connected = !mBluetoothHeadset.getConnectedDevices().isEmpty();
+                    onAudioEvent(context, connected);
+                    if(!connected){
+                        checkAD2PProfile(context, mBluetoothAdapter);
+                    }
+                }
+                // Close proxy connection after use.
+                mBluetoothAdapter.closeProfileProxy(profile, proxy);
             }
+
+            @Override
+            public void onServiceDisconnected(int profile) {
+                    //
+            }
+        };
+
+        // Establish connection to the proxy.
+        mBluetoothAdapter.getProfileProxy(context, mHeadsetProfileListener, BluetoothProfile.HEADSET);
+    }
+
+    private void checkAD2PProfile(final Context context, final BluetoothAdapter mBluetoothAdapter) {
+        BluetoothProfile.ServiceListener mA2DPProfileListener = new BluetoothProfile.ServiceListener() {
+
+            @Override
+            public void onServiceConnected(int profile, BluetoothProfile proxy) {
+                if(profile == BluetoothProfile.A2DP) {
+                    BluetoothA2dp mBluetoothA2dp = (BluetoothA2dp) proxy;
+                    onAudioEvent(context, !mBluetoothA2dp.getConnectedDevices().isEmpty());
+                }
+
+                // Close proxy connection after use.
+                mBluetoothAdapter.closeProfileProxy(profile, proxy);
+            }
+
+            @Override
+            public void onServiceDisconnected(int profile) {
+                //
+            }
+        };
+
+        mBluetoothAdapter.getProfileProxy(context, mA2DPProfileListener, BluetoothProfile.A2DP);
+    }
+
+    private void onAudioEvent(Context context, boolean connected){
+        if(connected){
+            onHeadSetPluggedIn(context);
+        } else {
+            onHeadSetPluggedOut(context);
         }
-    }*/
+    }
 
 }

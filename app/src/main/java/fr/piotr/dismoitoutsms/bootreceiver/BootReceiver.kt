a7ff16e7f6df52b3e91e73ebabd76f74ee59fd50
piotr <piotr.fleury@gmail.com>
2017-12-17 12:22:34 +0100

    bug fix for battery optimization

--- app/src/main/java/fr/piotr/dismoitoutsms/bootreceiver/BootReceiver.kt
+++ app/src/main/java/fr/piotr/dismoitoutsms/bootreceiver/BootReceiver.kt
@@ -3,8 +3,13 @@ package fr.piotr.dismoitoutsms.bootreceiver
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.os.Build
+import android.os.PowerManager
+import fr.piotr.dismoitoutsms.DisMoiToutSmsApplication
 
 import fr.piotr.dismoitoutsms.service.DisMoiToutSmsService
+import fr.piotr.dismoitoutsms.util.AbstractActivity
+import fr.piotr.dismoitoutsms.util.ConfigurationManager
 
 /**
  * Created by piotr on 09/07/2017.
@@ -14,8 +19,19 @@ import fr.piotr.dismoitoutsms.service.DisMoiToutSmsService
 class BootReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent) {
-        if (intent.action == Intent.ACTION_BOOT_COMPLETED) {
+        if (intent.action == Intent.ACTION_BOOT_COMPLETED && canStartService(context)) {
             context.startService(Intent(context, DisMoiToutSmsService::class.java))
         }
+    }
+
+    private fun canStartService(context: Context) : Boolean {
+        if (DisMoiToutSmsApplication.INSTANCE.disMoiToutSmsServiceRunning()) {
+            return false
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            val pm = context.getSystemService(AbstractActivity.POWER_SERVICE) as PowerManager
+            return pm.isIgnoringBatteryOptimizations(context.packageName)
+        }
+        return true
     }
 }
